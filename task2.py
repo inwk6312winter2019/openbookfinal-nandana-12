0dfb6d231c2f4e3318d31ee72aa76558327072c8
nandana-12 <nandana.srinivasan@dal.ca>
2019-02-15 11:16:19 -0800

    Task2 commit

---
+++ task2.py
@@ -0,0 +1,22 @@
+def create_log(fi,f1,f2,f3,f4):
+	for line in fi:
+		if("GET" in line)
+			f1.write(line)
+
+		elif("POST" in line)
+			f2.write(line)
+
+		elif("PUT" in line)
+			f3.write(line)
+		
+		elif("DELETE" in line)
+			f4.write(line)
+
+
+fi = open("access.log")
+f1 = open("get.log",'w')
+f2 = open("post.log",'w')
+f3 = open("put.log",'w')
+f4 = open("delete.log",'w')
+
+create_log(fi,f1,f2,f3,f4)
