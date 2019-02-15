4def6cd200a9524bfa84fd8a7fbb1a9ba3a03c36
nandana-12 <nandana.srinivasan@dal.ca>
2019-02-15 11:06:17 -0800

    Final commit

--- task1B.py
+++ task1B.py
@@ -7,24 +7,20 @@ def words(book):
 	li = []
 	punc = string.punctuation
 	for line in book:
-		line = line.strip()
+		line = line.strip(punc)
 		line = line.split()
 		for w in line:
-			for c in w:
-				if c not in punc:
-					l.append(w)
-	for wo in l:
-		d[w] = d.get(w,0) + 1
+			d[w] = d.get(w,0) + 1
 		for i,j in d.items(): 
 			li.append((j,i))
 		li.sort(reverse = True)
 	for i in range(0,21):
 		print(li[i][1])
 		
-
-
 f1 = open("Book1.txt")
-#f2 = open("Book2.txt")
-#f3 = open("Book3.txt")
+f2 = open("Book2.txt")
+f3 = open("Book3.txt")
 
 words(f1)
+words(f2)
+words(f3)
